44c9b8c7a95c93fc551a0074e04af70951f46c26
slls75966-try <slls75966@gmail.com>
2026-01-01 19:45:00 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = 'v1';
+const cacheAssets = [
+    'index.html',
+    'app.js',
+    'css/style.css',
+    'js/core.js',
+    'manifest.json',
+    'images/icon.png'
+];
+
+// Installation : Mise en cache des fichiers
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            console.log('Mise en cache des fichiers...');
+            return cache.addAll(cacheAssets);
+        })
+    );
+});
+
+// Stratégie : Répondre avec le cache, sinon chercher sur le réseau
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
